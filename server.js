35820334f302edf8ac510930d24251664cd21fb4
mshyam25 <shyammanikandan.22@gmail.com>
2022-05-20 11:00:32 +0530

    Removed upload routes

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-import path from 'path'
 import colors from 'colors'
 import express from 'express'
 import dotenv from 'dotenv'
@@ -7,7 +6,7 @@ import cors from 'cors'
 import { productRoutes } from './routes/productRoutes.js'
 import { userRoutes } from './routes/userRoutes.js'
 import { orderRoutes } from './routes/orderRoutes.js'
-import { uploadRoutes } from './routes/uploadRoutes.js'
+
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
@@ -18,13 +17,10 @@ app.use(cors())
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
-app.use('/api/upload', uploadRoutes)
+
 app.get('/paypalclient', async (request, response) => {
   response.send(process.env.PAYPAL_CLIENT_ID)
 })
-
-const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.use(notFound)
 app.use(errorHandler)
